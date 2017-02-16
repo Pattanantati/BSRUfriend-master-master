5a164ea8811b40445de77a1d2b728ecb7b04c7aa
champ <champ.chim@gmail.com>
2017-02-16 16:06:30 +0700

    16feb

--- app/src/main/java/appewtc/masterung/bsrufriend/ServiceActivity.java
+++ app/src/main/java/appewtc/masterung/bsrufriend/ServiceActivity.java
@@ -1,7 +1,16 @@
 package appewtc.masterung.bsrufriend;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,17 +22,104 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class ServiceActivity extends FragmentActivity implements OnMapReadyCallback {
     //ewplicit
     private GoogleMap mMap;
-    private double uesrLatDouble = 13.733030, userLngDouble = 100.489406;
+    private double userLatDouble = 13.733030, userLngDouble = 100.489406;
+    private TextView textView;
+    private Button button;
+    private String[] loginStrings;
+    private LocationManager locationManager;
+    private Criteria criteria;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_layout);
+
+        //Bind Widget
+        textView = (TextView) findViewById(R.id.textView2);
+        button = (Button) findViewById(R.id.button4);
+
+        //recive value for mainActivity
+        loginStrings = getIntent().getStringArrayExtra("Login");
+
+        //show text
+        textView.setText(loginStrings[1]);
+
+        //setup location
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }//main method
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.removeUpdates(locationListener);
+    }
+
+    public Location myFindLocation(String strProvider) {
+
+        Location location = null;
+
+        if (locationManager.isProviderEnabled(strProvider)) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return null;
+            }
+            locationManager.requestLocationUpdates(strProvider, 1000, 10, locationListener);
+            location = locationManager.getLastKnownLocation(strProvider);
+
+        } //if
+        return location;
+    }
+
+    public LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            userLatDouble = location.getLatitude();
+            userLngDouble = location.getLongitude();
+
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
 
 
     @Override
@@ -32,6 +128,6 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
 
         //setup center of map
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom
-                (new LatLng(uesrLatDouble,userLngDouble),16));
+                (new LatLng(userLatDouble,userLngDouble),16));
     }// on mapready
 } //Main class
